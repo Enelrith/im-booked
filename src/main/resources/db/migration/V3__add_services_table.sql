50c480cf92dbf256f25235f64fd2b15b1e5d5b6d
Enelrith <dimarelefkada@gmail.com>
2026-02-27 19:05:05 +0200

    feat: add services table migration

---
+++ src/main/resources/db/migration/V3__add_services_table.sql
@@ -0,0 +1,11 @@
+create table services(
+    id binary(16) primary key default (uuid_to_bin(uuid())),
+    name varchar(100) not null,
+    price decimal(10, 2) not null,
+    duration_minutes integer not null,
+    is_active boolean not null default true,
+    business_id binary(16) not null,
+    constraint ck_services_price_positive_or_zero check (price >= 0),
+    constraint ck_services_duration_minutes_positive check (duration_minutes > 0),
+    constraint fk_services_businesses foreign key (business_id) references businesses (id)
+)
